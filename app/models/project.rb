a4a86692ede51f1d0160c81b593fd901ca69e249
olindos <yason4ik87@gmail.com>
2020-10-07 16:20:50 +0300

    Add validation to model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  has_many :tasks, dependent: :delete_all
+  belongs_to :user
+  validates :name, presence: :true, uniqueness: { scope: :user_id , length: { maximum: 50 } }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :project
+  validates :name, presence: :true, uniqueness: { scope: :project_id, length: { maximum: 150 } }
 end
