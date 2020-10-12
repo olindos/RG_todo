7a6a701936e7dec6c64933c37df1bd15fe322f23
olindos <yason4ik87@gmail.com>
2020-10-12 16:19:55 +0300

    Add default value to status

--- db/migrate/20201006111831_create_tasks.rb
+++ db/migrate/20201006111831_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.boolean :status, default: false
+      t.boolean :status
       t.references :project, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_030003) do
+ActiveRecord::Schema.define(version: 2020_10_09_170118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_030003) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.boolean "status"
+    t.boolean "status", default: false
     t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
