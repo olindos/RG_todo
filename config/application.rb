cda9f6a671c3849b340b8b82b629316cb705aa86
olindos <yason4ik87@gmail.com>
2020-11-02 12:24:25 +0200

    fix error

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,8 @@ Bundler.require(*Rails.groups)
 
 module RGTodo
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.action_dispatch.default_headers.merge!('Cache-Control' => 'no-store, no-cache')
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
   end
 end
