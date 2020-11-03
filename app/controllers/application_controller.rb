8ed25b2b5134e6a464e1827f543d00bc5c89e445
olindos <yason4ik87@gmail.com>
2020-11-03 09:42:49 +0200

    devise exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+    rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    sign_out_user # Example method that will destroy the user cookies
+  end
 end
